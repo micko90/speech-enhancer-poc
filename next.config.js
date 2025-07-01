ee7fc49a9f97d1b54e714b20bdf67e22e3528da8
micko90 <32385992+micko90@users.noreply.github.com>
2025-07-01 17:48:11 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,5 @@
+// next.config.js
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  api: {
-    bodyParser: false // we use formidable for multipart
-  }
-};
+const nextConfig = {};
+
 module.exports = nextConfig;
